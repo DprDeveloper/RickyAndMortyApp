f40e425227620f5e4728dbf7ca775572d9532baa
David <davidpozorojas@gmail.com>
2021-10-13 20:08:33 +0200

    Implement recyclerview adapter

--- app/src/main/java/com/dprddeveloper/rickyandmortyapp/MainActivity.kt
+++ app/src/main/java/com/dprddeveloper/rickyandmortyapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.dprddeveloper.rickyandmortyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dprddeveloper.rickyandmortyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(view.root)
     }
 }
---
+++ app/src/main/java/com/dprddeveloper/rickyandmortyapp/adapter/RmAdapter.kt
@@ -0,0 +1,31 @@
+package com.dprddeveloper.rickyandmortyapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dprddeveloper.rickyandmortyapp.R
+import com.dprddeveloper.rickyandmortyapp.databinding.RmListItemBinding
+import com.dprddeveloper.rickyandmortyapp.loadUrl
+import com.dprddeveloper.rickyandmortyapp.model.RmCharacter
+
+class RmAdapter(val rmItems: List<RmCharacter>): RecyclerView.Adapter<RmAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.rm_list_item, parent,false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(rmItems[position])
+    }
+
+    override fun getItemCount(): Int = rmItems.size
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        val view = RmListItemBinding.bind(view)
+        fun bind(item: RmCharacter){
+            view.name.text = item.name
+            view.image.loadUrl(item.url)
+        }
+    }
+}
